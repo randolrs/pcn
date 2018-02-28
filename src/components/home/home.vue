64faadd3db083a5291cca1dd1504b74390343d80
randolrs <r.shane.randolph@gmail.com>
2018-02-28 09:00:43 -0500

    style iframe

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
-  div.content__main
-    h1
-      span 5150
-    iframe(width="560" height="315" src="https://www.youtube.com/embed/g6GRNuDwSAE?start=117&end=144" frameborder="0" allowfullscreen allow="encrypted-media;")
+div.content__main
+  h1
+    span 5150
+  .iframe__container: .iframe__container__wrapper: iframe(width="560" height="315" src="https://www.youtube.com/embed/g6GRNuDwSAE?start=117&end=144" frameborder="0" allowfullscreen allow="encrypted-media;")
 
 </template>
 
