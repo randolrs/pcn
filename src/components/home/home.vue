984ad67b88f69fadd80746b231ed17423213e476
randolrs <r.shane.randolph@gmail.com>
2018-02-28 08:25:24 -0500

    add iframe clip

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -2,6 +2,7 @@
   div.content__main
     h1
       span 5150 Nation
+    iframe(width="560" height="315" src="https://www.youtube.com/embed/g6GRNuDwSAE?start=117&end=144" frameborder="0" allowfullscreen)
 
 </template>
 
