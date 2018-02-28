9ef1fece9bd4151481b0cd6ca728e47321388db4
randolrs <r.shane.randolph@gmail.com>
2018-02-28 08:37:52 -0500

    add encrypted-media arg to iframe

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
   div.content__main
     h1
-      span 5150 Nation
-    iframe(width="560" height="315" src="https://www.youtube.com/embed/g6GRNuDwSAE?start=117&end=144" frameborder="0" allowfullscreen)
+      span 5150
+    iframe(width="560" height="315" src="https://www.youtube.com/embed/g6GRNuDwSAE?start=117&end=144" frameborder="0" allowfullscreen allow="encrypted-media;")
 
 </template>
 
